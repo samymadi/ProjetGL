c7ed0019e787fd9e09e7b88a0661f4b11fc44038
SaMy <samymadi187@gmail.com>
2020-09-23 12:32:59 +0100

    FirstCommit

---
+++ Client.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+
+
+
+public class ClientLogin{
+         
+    public ClientLogin(String Username ,String Password ){
+        
+        this.Username= Username;
+        this.Password= Password;
+    }
+        protected long LoginId;
+        protected long ClientId;
+        protected String Username;
+        protected String Password;
+        
+    protected Boolean Connected(){
+            AccesDataBase();
+              
+    }           
+}
+
+
+
+
+
---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+
+
+
+
+class Main{
+
+    public static void main( String[]  args){
+        System.out.println("Hello World et sam");
+        
+        Client c = new Client();
+    
+    }
+}
+
+
+
+ class Client{
+
+}
